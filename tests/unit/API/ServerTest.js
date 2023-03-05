08aaeb40377782508fee26aa0e14c32ef0c15908
Rodrigo Cinto <rodcinto@gmail.com>
2023-03-05 14:01:17 +0100

    Move Server into API folder.

---
+++ tests/unit/API/ServerTest.js
@@ -0,0 +1,37 @@
+const {describe, it, beforeEach} = require('mocha');
+const sinon = require('sinon');
+const npmlog = require('npmlog');
+const express = require('express');
+
+let Server;
+
+beforeEach(() => {
+  Server = require('../../../build/src/API/server');
+});
+
+describe('Server', () => {
+  it('should listen', () => {
+    const app = express();
+    const listenStub = sinon.stub(app, 'listen').returns(() => {});
+
+    const loggerMock = sinon.mock(npmlog);
+    loggerMock.expects('info').once();
+
+    const appServer = new Server.default(app, loggerMock);
+    appServer.start();
+    sinon.assert.calledOnce(listenStub);
+  });
+
+  it('should log error', () => {
+    const app = express();
+    sinon.stub(app, 'use').throws(new Error('Bad Configuration'));
+    sinon.stub(app, 'listen').returns(() => {});
+
+    const loggerMock = sinon.mock(npmlog);
+    loggerMock.error = () => {};
+    loggerMock.expects('error').once();
+
+    const appServer = new Server.default(app, loggerMock);
+    appServer.start();
+  });
+});
