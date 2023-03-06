659e112a45e417c25c3cbf7e4175e69b79e16b09
Rodrigo Cinto <rodcinto@gmail.com>
2023-03-06 10:01:37 +0100

    Fix typo in Email tests.

--- tests/unit/Person/Domain/EmailTest.js
+++ tests/unit/Person/Domain/EmailTest.js
@@ -9,14 +9,14 @@ beforeEach(() => {
 });
 
 describe('Person Email', () => {
-  it('should should sanitize', () => {
+  it('should sanitize', () => {
     const emailReference = faker.internet.email();
 
     const email = new Email.default(emailReference.toUpperCase());
     chai.assert.equal(email.getValue(), emailReference.toLowerCase());
   });
 
-  it('should should throw error when email is invalid', () => {
+  it('should throw error when email is invalid', () => {
     chai.assert.throws(() => {
       new Email.default('Bad Email@domain.com');
     }, Error);
