3e0446e3854d822930859bb6af96b7e7268c8f7b
Rodrigo Cinto <rodcinto@gmail.com>
2023-03-07 15:24:36 +0100

    Remove residual mongoose config from app.

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,8 @@
 import express, {Express, Request, Response} from 'express';
 import npmlog from 'npmlog';
-import mongoose, {Connection, Schema} from 'mongoose';
-import config from 'config';
 
 import Server from './API/Server';
 import Router from './API/Router';
-
-// Connect to MongoDB
-mongoose.connect(config.get('DBHost'), {
-  dbName: config.get('DBName'),
-  user: config.get('DBUser'),
-  pass: config.get('DBPass'),
-});
-
-const db: Connection = mongoose.connection;
-db.on('error', npmlog.error.bind(console, 'CONNECTION ERROR:'));
 
 const app: Express = express();
 const server = new Server(app, npmlog);
